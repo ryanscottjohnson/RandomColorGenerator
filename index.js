94054cfb967699c0501b64adaf01980e1aeb4eea
ryanscottjohnson <teacherman1@gmail.com>
2018-08-27 15:10:52 -0700

    built out color generator functions in index.js file

--- index.js
+++ index.js
@@ -5,12 +5,52 @@ import { Button } from './Button.js';
 class Random extends React.Component {
   constructor(props) {
     super(props);
-
+    this.state = { color: [] };
+    this.handleClick = this.handleClick.bind(this);
   }
+  componentDidMount(){
+    this.newColor();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    this.newColor();
+  }
+
+  formatColor(array) {
+    return 'rgb(' + array.join(', ') + ')';
+  }
+
+  isLight(){
+    const rgb = this.state.color;
+    return rgb.reduce((a,b) => a+b) < 127 * 3;
+  }
+
+  applyColor() {
+    const color = this.formatColor(this.state.color);
+    document.body.style.background = color;
+  }
+
+  chooseColor() {
+    const random = [];
+    for(let i = 0; i < 3; i++){
+      random.push(Math.floor(Math.random() * 256));
+    }
+  }
+
+  handleClick() {
+    this.setState({
+      color : this.chooseColor()
+    });
+  }
+
 
   render(){
     return(
-
+      <div>
+        <h2 className={this.isLight() ? 'white' : 'black'}>Color {this.formatColor(this.state.color)}!
+        </h2>
+        <Button light={this.isLight()} onClick={this.handleClick} />
+      </div>
     );
   }
 }
