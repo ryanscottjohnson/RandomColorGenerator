d62e538214adeb1604aad5f3cb7b13b05707796c
ryanscottjohnson <teacherman1@gmail.com>
2018-10-05 12:40:16 -0700

    colorGenegator re-write

--- index.js
+++ index.js
@@ -1,61 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Button } from './Button.js';
+function generate() {
 
-class Random extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { color: [] };
-    this.handleClick = this.handleClick.bind(this);
+  let hexValues = ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e"];
+  let newColor = "#";
+  
+  for ( let i = 0; i < 6; i++ ) {
+    let x = Math.round( Math.random() * 14 );
+    let y = hexValues[x];
+    newColor += y;
   }
-  componentDidMount(){
-    this.newColor();
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    this.newColor();
-  }
-
-  formatColor(array) {
-    return 'rgb(' + array.join(', ') + ')';
-  }
-
-  isLight(){
-    const rgb = this.state.color;
-    return rgb.reduce((a,b) => a+b) < 127 * 3;
-  }
-
-  applyColor() {
-    const color = this.formatColor(this.state.color);
-    document.body.style.background = color;
-  }
-
-  chooseColor() {
-    const random = [];
-    for(let i = 0; i < 3; i++){
-      random.push(Math.floor(Math.random() * 256));
-    }
-  }
-
-  handleClick() {
-    this.setState({
-      color : this.chooseColor()
-    });
-  }
-
-
-  render(){
-    return(
-      <div>
-        <h2 className={this.isLight() ? 'white' : 'black'}>Color {this.formatColor(this.state.color)}!
-        </h2>
-        <Button light={this.isLight()} onClick={this.handleClick} />
-      </div>
-    );
-  }
+  
+  document.getElementById("container").style.backgroundColor = newColor;
+  document.getElementById("output").innerHTML = newColor;
+  
 }
 
-ReactDOM.render(
-  <Random />,
-  document.getElementById('app')
-);
+generate();
