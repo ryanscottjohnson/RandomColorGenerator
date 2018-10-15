235b638b3a351809ec9c62351acfb9bae1776f81
ryanscottjohnson <teacherman1@gmail.com>
2018-10-15 15:21:49 -0700

    revision step 2

--- Button.js
+++ Button.js
@@ -1,13 +1,13 @@
-const React = require('react');
+import React from 'react';
 
-let Button = React.createClass({
-  render: function () {
-    return (
+class Button extends React.Componet{
+  render() {
+    return <React.Fragment>
       <button className={this.props.light ? 'light-button' : 'dark-button' } onClick={this.props.onClick}>
         Click Me!
       </button>
-    );
+    </React.Fragment>
   }
-});
+};
 
-module.exports = Button;
+export default Button;
