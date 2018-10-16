b78f847f61d6824ef7b3c61fe953ffea7f8c5a20
ryanscottjohnson <teacherman1@gmail.com>
2018-10-16 13:01:29 -0700

    restructure app

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Random from './components/Random.jsx';
+
+let root = document.createElement('div');
+document.body.appendChild(root);
+
+ReactDOM.render(<Random />, root);
